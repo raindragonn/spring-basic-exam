134754e68254d3c81ceefa116e4f7af77f2b30f8
raindragonn <raindragonn@gmail.com>
2023-08-11 18:43:07 +0900

    test: ObjectProvider를 통한 DL활용하기

--- src/test/java/raindragonn/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/raindragonn/core/scope/SingletonWithPrototypeTest1.java
@@ -4,13 +4,13 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SingletonWithPrototypeTest1 {
 
@@ -40,26 +40,24 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
+    @RequiredArgsConstructor
     static class ClientBean {
-        private final PrototypeBean prototypeBean; // 생성 시점에 주입됨.
 
-        @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private final ObjectProvider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
     }
 
     @Scope("prototype")
-    @ToString
     static class PrototypeBean {
         private int count = 0;
 
@@ -73,7 +71,7 @@ public class SingletonWithPrototypeTest1 {
 
         @PostConstruct
         void init() {
-            System.out.println("PrototypeBean.init" + this);
+            System.out.println("PrototypeBean.init " + this);
         }
 
         @PreDestroy
