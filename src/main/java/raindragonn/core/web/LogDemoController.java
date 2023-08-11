e61c508efdc8bf3b276c61aafdfa3c86b7de685c
raindragonn <raindragonn@gmail.com>
2023-08-11 19:40:43 +0900

    feat: DL을 통한 request스코프 빈 요청 지연하기

---
+++ src/main/java/raindragonn/core/web/LogDemoController.java
@@ -0,0 +1,30 @@
+package raindragonn.core.web;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import raindragonn.core.common.MyLogger;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        MyLogger myLogger = myLoggerProvider.getObject();
+        String requestURL = request.getRequestURL().toString();
+
+        myLogger.setRequestURL(requestURL);
+        myLogger.log("controller test");
+
+        logDemoService.logic("testID");
+        return "OK";
+    }
+}
